b17725cfcb18fef6e6c6107b49c8e7bc80b4fde4
H3artbrake <77528577+H3artbrake@users.noreply.github.com>
2026-09-05 21:48:12 +0900

    Add editable workout targets and native mobile rep picker

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='repbook-shell-v9';
+const CACHE='repbook-shell-v1.0.1';
 const FILES=['/','/index.html','/style.css','/app.js','/logic.js','/manifest.webmanifest','/icon-192.png','/icon-512.png','/apple-touch-icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
